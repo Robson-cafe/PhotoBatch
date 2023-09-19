dce2f6f0e52a0a6f040a1c6eed8c7a642116585c
Robson Silva <robsonveloso@hotmail.com>
2023-09-18 22:17:21 -0300

    #Commit 02 - Lendo argumentos da main

--- PhotoBatch/PhotoBatch.cpp
+++ PhotoBatch/PhotoBatch.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 
-int main()
+//int main()
+//{
+//	setlocale(LC_ALL, "pt_BR");
+//
+//	std::cout << "Hello World!!!" << std::endl;
+//	std::cout << "Olá Mundo!!!" << std::endl;
+//
+//	return 0;
+//}
+
+int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "pt_BR");
 
-	std::cout << "Hello World!!!" << std::endl;
-	std::cout << "Olá Mundo!!!" << std::endl;
+	for (int i = 0; i < argc; ++i) {
+		std::cout << argv[i] << std::endl;
+	}
 
+	//getchar();
 	return 0;
 }
