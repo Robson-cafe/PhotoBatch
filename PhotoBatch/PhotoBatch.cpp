5ac4321f43aed36aa4c06ee730c698a8e54e2373
Robson Silva <robsonveloso@hotmail.com>
2023-09-19 20:58:36 -0300

    #Commit 04 - Implementando parse e case sensitive;

--- PhotoBatch/PhotoBatch.cpp
+++ PhotoBatch/PhotoBatch.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,6 +14,18 @@ using namespace std;
 //
 //	return 0;
 //}
+
+string ToLower(
+	string str
+) 
+{
+	/*for (char& c : str) {
+		c = tolower(c);
+	}*/
+
+	transform(begin(str), end(str), begin(str), [](unsigned char c) {return tolower(c); });
+	return str;
+}
 
 class ArgumentParser
 {
@@ -46,7 +59,36 @@ class ArgumentParser
 			char* argv[]
 		)
 		{
+			if (argc > 1) {
+				for (int i = 1; i < argc; ++i) {
+					//string arg = argv[i];
+					string arg = ToLower(argv[i]);
 
+					if (arg.length() >= 3) {
+						//argumentos tem que ter pelo menos 3 caracteres
+						// os "--" iniciais e pelo menos um terceiro caracter
+						if (arg[0] == '-' && arg[1] == '-') {
+							//--rename
+							//primeiro remover os "--" iniciais
+							arg = arg.substr(2);
+
+							//depois do arg.substr: "rename"
+							if (arg.find_first_of('=') != string::npos) {
+								//é uma opção
+							}
+							else {
+								// é uma flag
+								auto flagIt = m_Flags.find(arg);
+
+								if (flagIt != end(m_Flags)) {
+									// encontrou a flag
+									flagIt->second = true;
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 
 	private:
